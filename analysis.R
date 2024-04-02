649cbe2de5a6327c8153478cf50f7bfb9e5a3d4f
jtlandis <jtlandis314@gmail.com>
2024-04-02 13:44:58 -0400

    refactoring animation to its own section

--- analysis.R
+++ analysis.R
@@ -4,29 +4,6 @@ box::use(mods/setup_SLICER[...],
          lle[lle],
          tibble[tibble, as_tibble])
 
-# as_seq <- function(vec) {
-#   f <- factor(vec)
-#   levels(f) <- seq_along(levels(f))
-#   as.integer(f)
-# }
-# calc_vertex_freqs <- function(path_counts, path_len) {
-#   n <- length(path_len)
-#   vertex_freqs <- matrix(0, n, n)
-#   for (i in seq_len(n)) {
-#     vertex_freqs[path_len[i], i] <- path_counts[i]
-#   }
-#   vertex_freqs
-# }
-#
-# calc_path_counts <- function(vpath, which_ind) {
-#   counts <- integer(length(which_ind))
-#   for (i in seq_along(vpath)) {
-#     index <- which_ind[vpath[[i]]]
-#     counts[index] <- counts[index] + 1L
-#   }
-#   counts
-# }
-
 data <- suppressMessages(h5ad2SE(box::file("data/adata_olsson.h5ad")))
 ptraj <- t(assay(data, "counts"))
 
@@ -43,14 +20,11 @@ d <- tibble(x = traj_lle[,1],
             # z = traj_lle[,3],
             branch = factor(branches),
             order = order(cells_ordered),
-            cell_type = colData(data)$cell_type) #|>
-  # dplyr::left_join(y = cell_o, by = c("i"="cell_i"))
+            cell_type = colData(data)$cell_type)
+
 aricode::NMI(d$branch, d$cell_type)
 table(d$branch, d$cell_type)
-box::use(
-  ggplot2[...],
-  gganimate[...]
-)
+box::use(ggplot2[...])
 p <-  d |>
   dplyr::arrange(order) |>
   ggplot(aes(x, y)) +
@@ -58,7 +32,10 @@ p <-  d |>
   geom_point(aes(color = branch, group = order), size = 2) +
   labs(x = "Manifold 1", y = "Manifold 2", title = "Cell Ordering") +
   theme_bw()
+p
 
+## Animation ----
+box::use(gganimate[...])
 anim <- p + transition_components(order, enter_length = 50L, exit_length = 50L) +
   shadow_mark(exclude_layer = 1) +
   enter_grow() +
@@ -69,6 +46,7 @@ anim <- p + transition_components(order, enter_length = 50L, exit_length = 50L) 
 anim_save(box::file("prelim_data.gif"),
           animation = anim, duration = 20, fps = 30)
 
+## DESeq2 ----
 
 box::use(DESeq2[...])
 do_deseq <- function(data, design) {
